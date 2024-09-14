a4c4b18725f09fc92e29944f6d8b415194472cfe
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 11:53:33 +0530

    Created

---
+++ src/test/java/org/sam/ManualTest.java
@@ -0,0 +1,17 @@
+package org.sam;
+
+public class ManualTest {
+	private void name1() {
+		System.out.println(34);
+		
+	}
+	private void name2() {
+		System.out.println(56);
+		
+		
+	}
+	private void name3() {
+		System.out.println(57);
+}
+
+}
