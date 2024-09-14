a74b6a39bb3298ce4a4eaefad0397ead3e13c45c
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 12:04:06 +0530

    code added

--- src/test/java/org/sam/ManualTest.java
+++ src/test/java/org/sam/ManualTest.java
@@ -13,5 +13,17 @@ public class ManualTest {
 	private void name3() {
 		System.out.println(57);
 }
+	private void name4() {
+		System.out.println(34);
+		
+	}
+	private void name5() {
+		System.out.println(56);
+		
+		
+	}
+	private void name6() {
+		System.out.println(57);
+}
 
 }
